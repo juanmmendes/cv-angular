a26579768985bfba36cf8221ec5e7ab88d29cbab
Juan Mendes <juan.zx016@gmail.com>
2025-10-18 23:53:46 -0300

    Deploy dist build

--- src/main.server.ts
+++
@@ -1,7 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/app.component';
-import { appConfigServer } from './app/app.config.server';
-
-const bootstrap = () => bootstrapApplication(AppComponent, appConfigServer);
-
-export default bootstrap;
